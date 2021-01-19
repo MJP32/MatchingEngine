f7488ae4f674c57b943ccc36a96f9a79ad8c278c
michael <michaelperera32@gmail.com>
2021-01-19 02:13:04 -0500

    initial commit

--- src/main/java/OrderType.java
+++
@@ -1,9 +0,0 @@
-public enum OrderType {
-    market, limit;
-
-    private OrderType type;
-
-    public OrderType getOrderType() {
-        return type;
-    }
-}
--- src/main/java/matching/LimitOrder.java
+++
@@ -1,9 +0,0 @@
-package matching;
-
-import java.math.BigDecimal;
-
-public class LimitOrder extends Order{
-    public LimitOrder(String symbol, String side, String type, BigDecimal price, Long timeStamp) {
-        super(symbol, side, type, price, timeStamp);
-    }
-}
